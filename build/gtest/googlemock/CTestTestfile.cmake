89f572f3cd05b7cce118dbd219c1ebb460b773ae
andreimiculita <andreimiculita@gmail.com>
2019-10-09 16:33:12 -0700

    Deleted all files so we can start from the initial structure

--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/cognitiverobotics/student_ws/rug_kfold_cross_validation/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
